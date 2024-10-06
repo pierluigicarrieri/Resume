b098819f8502a0ae3d211f328ee1aa93c71e9f5e
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2024-10-06 23:44:37 +0200

    Done data structures

--- content.php
+++ content.php
@@ -24,17 +24,124 @@ $ita = [
                         sono sempre pronto a mettere alla prova le mie capacità e ad acquisirne di nuove.',
 
     'experience' => [
-        'title' => 'ESPERIENZA'
-        
+        'title' => 'ESPERIENZA',
+        'content' => [
+            [
+                'title' => 'Sviluppo e personalizzazione di ERP',
+                'content' => 'Gestito lo sviluppo di un ERP modulare, utilizzato come template per diversi progetti aziendali, 
+                                personalizzato per settori come pulizia industriale e istruzione. 
+                                Contributo sia al template principale sia alle sue iterazioni specifiche.'
+            ],
+            [
+                'title' => 'Creazione del modulo di gestione magazzino',
+                'content' => 'Ideato, sviluppato e portato a termine in completa autonomia il modulo di gestione del magazzino 
+                                all\'interno dell\'ERP, con particolare attenzione alla gestione delle scorte, inventario e ordini.'
+            ],
+            [
+                'title' => 'Manutenzione e miglioramenti su e-commerce',
+                'content' => 'Gestito la manutenzione di un sito e-commerce B2B per la vendita all’ingrosso di parti di auto 
+                                e camper, con implementazioni su gestione clienti, agenti di vendita e ordini. Esperienza anche 
+                                nella creazione e design di siti web in WordPress.'
+            ]
+        ]
+    ],
+
+    'courses_and_training' => [
+        'title' => 'CORSI E MASTER',
+        'content' => 'Bootcamp di 700 ore teoriche e pratiche durante il quale ho avuto modo di studiare 
+                    e padroneggiare tecnologie frontend e backend.'
+    ],
+
+    'education' => [
+        'title' => 'FORMAZIONE',
+        'content' => [
+            [
+                'title' => 'C2 Proficiency',
+                'body' => 'British Council'
+            ],
+            [
+                'title' => 'Laurea in Giurisprudenza',
+                'body' => 'Università di Bologna'
+            ],
+            [
+                'title' => 'Diploma di Maturità scientifica',
+                'body' => 'Liceo Scientifico San Vincenzo De\' Paoli'
+            ],
+        ]
     ]
     
 ];
 
 
 
-
-
-
 $eng = [
 
+    'contact_info' => [
+        'mail' => 'pierluigi.carrieri.90@gmail.com',
+        'phone' => '348 06 35 960',
+        'address' => 'Bologna (BO), Italia',
+        'linkedin' => 'www.linkedin.com/in/pierluigi-carrieri-2489a424a',
+        'github' => 'https://github.com/pierluigicarrieri'
+    ],
+
+    'interests' => [
+        'title' => 'INTERESTS',
+        'content' => [
+            'Literature',
+            'Cinema',
+            'Sports'
+        ]
+    ],
+
+    'personal_info' => 'I am a junior web developer always looking for challenges and new experiences.
+                        Programming and communication with people from all over the world thrill me: 
+                        I am always ready to put myself to the test and to learn something new.',
+
+    'experience' => [
+        'title' => 'WORK EXPERIENCE',
+        'content' => [
+            [
+                'title' => 'ERP Development and Customization',
+                'content' => 'Managed the development of a modular ERP, used as a template for various business projects, 
+                            customized for industries such as industrial cleaning and education. Contributed to both the main template 
+                            and its specific iterations.'
+            ],
+            [
+                'title' => 'Creation of a Warehouse Management Module',
+                'content' => 'Designed, developed, and completed independently the warehouse management module within the ERP, 
+                            with a focus on stock management, inventory, and orders.'
+            ],
+            [
+                'title' => 'Maintenance and Enhancements on E-commerce',
+                'content' => 'Managed the maintenance of a B2B e-commerce site for the wholesale sale of auto and RV parts, 
+                            implementing features for customer management, sales agents, and orders. Also experienced in 
+                            creating and designing websites in WordPress.'
+            ]
+        ]
+    ],
+
+    'courses_and_training' => [
+        'title' => 'COURSES AND TRAINING',
+        'content' => '700-hour theoretical and practical bootcamp, during which I had the opportunity to study and master 
+                    frontend and backend technologies.'
+    ],
+
+    'education' => [
+        'title' => 'EDUCATION',
+        'content' => [
+            [
+                'title' => 'C2 Proficiency',
+                'body' => 'British Council'
+            ],
+            [
+                'title' => 'Law Degree',
+                'body' => 'University of Bologna'
+            ],
+            [
+                'title' => 'High School Graduation',
+                'body' => 'Liceo Scientifico San Vincenzo De\' Paoli'
+            ],
+        ]
+    ]
+    
 ];
